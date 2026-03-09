1df3b639a897719fbd0e8dcd832ca07be4e11403
Sumin Kim <sumin002@gmail.com>
2026-03-09 10:33:57 -0400

    todolist with delete function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("created");
-    return () => console.log("destroyed");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
+import Btn from "./Button";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [todo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setToDos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
+  const onDelete = (index) => {
+    setToDos((current) => current.filter((_, i) => i !== index));
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My Todo List : ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="Put your todo"
+        ></input>
+        <button>Add</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <Btn text="Delete" onClick={() => onDelete(index)} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
